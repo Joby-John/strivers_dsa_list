6bdbcefd000a4b8e3e660d48e5bed27742222913
Joby-John <joby432john@gmail.com>
2024-06-08 11:12:56 +0530

    string getline

---
+++ stringGetline.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string intro;
+
+    //cin>>intro;// gets only til first space
+    getline(cin, intro); //get the whole line
+    cout<<intro;
+
+    return 0;
+}
