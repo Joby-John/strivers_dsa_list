7b123329eb5cec4bc15d274472b9987f6a790e59
Joby-John <joby432john@gmail.com>
2024-06-13 11:53:51 +0530

    added explanation and solution for follow up qn

--- maximumSubarray53Kedans.cpp
+++ maximumSubarray53Kedans.cpp
@@ -1,3 +1,5 @@
+// follow up qn to print the index of the array elements that constitute the max subarray
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -7,18 +9,26 @@ int kedan(vector<int>&nums)
     int n = nums.size();
     int max = nums[0];
     int sum = 0;
+    int startIndex = -1; //for follow up qn
+    int endIndex = -1;// for follow up qn
 
     for (int i = 0; i<n; i++){
+
+        
         sum+=nums[i];
 
         if(sum>max){
             max = sum;
+            endIndex = i; // for follow up qn
+            
         }
         if(sum<0)
         {
             sum = 0;
+            startIndex = i+1; // for follow up qn, as sum resets everytime when sum gets negative
         }
     }
+    cout<< startIndex<<","<< endIndex<<endl;
     return max;
 }
 
