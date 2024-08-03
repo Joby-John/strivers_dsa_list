33760973bb8b69e9c645eff95b3c060bf07c7d90
Joby-John <joby432john@gmail.com>
2024-08-03 08:45:53 +0530

    Both the questions that were asked for zoho round 2

--- second.cpp
+++ second.cpp
@@ -1,33 +1,58 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int main()
-{
-    string s; 
-    cin>>s;
+int main() {
+    string s;
+    cin >> s;
     int n = s.length();
-    vector<int>numbers;
-    vector<string>operators;
-
+    vector<int> numbers;
+    vector<char> operators;
+    
     string num = "";
-
-    for(int i = 0; i<n; i++)
-    {
-        if(s[i] != '+' && s[i] != '*' && s[i] !='/' && s[i]!='-')
-        {
-            num = num+s[i];
-        }
-        else{
+    
+    // Extract numbers and operators
+    for (int i = 0; i < n; i++) {
+        if (isdigit(s[i])) {
+            num += s[i];
+        } else {
             numbers.push_back(stoi(num));
             num = "";
-            string symbol;
-            symbol = s[i];
-            operators.push_back(symbol);
+            operators.push_back(s[i]);
         }
-
     }
     numbers.push_back(stoi(num));
-
-
+    
+    // Evaluate * and / first
+    for (int i = 0; i < operators.size(); i++) {
+        if (operators[i] == '*' || operators[i] == '/') {
+            int operand1 = numbers[i];
+            int operand2 = numbers[i + 1];
+            int result;
+            
+            if (operators[i] == '*') {
+                result = operand1 * operand2;
+            } else if (operators[i] == '/') {
+                result = operand1 / operand2;
+            }
+            
+            // Update the numbers and operators
+            numbers[i] = result;
+            numbers.erase(numbers.begin() + i + 1);
+            operators.erase(operators.begin() + i);
+            i--; // Adjust index after modification
+        }
+    }
+    
+    // Now evaluate + and -
+    int result = numbers[0];
+    for (int i = 0; i < operators.size(); i++) {
+        if (operators[i] == '+') {
+            result += numbers[i + 1];
+        } else if (operators[i] == '-') {
+            result -= numbers[i + 1];
+        }
+    }
+    
+    cout << result;
+    return 0;
 }
